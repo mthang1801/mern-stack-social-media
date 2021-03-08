5fc6d556dce03a4900bf21b7e89d1af45972bba2
mthang1801 <mthang1801@gmail.com>
2021-03-09 00:57:16 +0700

    show messengers to screen message chat page

--- frontend/src/components/Chat/ListMessages.jsx
+++ frontend/src/components/Chat/ListMessages.jsx
@@ -1,25 +1,38 @@
-import React, {useEffect, useState} from 'react'
-import {ListMessagesWrapper} from "./styles/ListMessages.styles"
-import {useQuery} from "@apollo/client"
-import {GET_CURRENT_USER, GET_MESSAGES_STORAGE} from "../../apollo/operations/queries/cache"
-import {FETCH_FRIENDS} from "../../apollo/operations/queries/user"
-import MessageItem from "./MessageItem"
-import {usePopupMessagesActions} from "./hook/usePopupActions"
+import React, { useEffect, useState } from "react";
+import { ListMessagesWrapper } from "./styles/ListMessages.styles";
+import { useQuery } from "@apollo/client";
+import {
+  GET_CURRENT_USER,
+  GET_MESSAGES_STORAGE,
+  GET_CURRENT_CHAT,
+} from "../../apollo/operations/queries/cache";
+import { FETCH_FRIENDS } from "../../apollo/operations/queries/user";
+import MessageItem from "./MessageItem";
+import { usePopupMessagesActions } from "./hook/usePopupActions";
 const ListMessages = () => {
-  const {data : {user}} = useQuery(GET_CURRENT_USER);
-  const {data : {messagesStorage}} = useQuery(GET_MESSAGES_STORAGE, {fetchPolicy : "cache-and-network"});
-  const {refetch: fetchFriends} = useQuery(FETCH_FRIENDS, {skip :true});  
-  
-  const {setShowPopup} = usePopupMessagesActions()
-  
-  if(!Object.keys(messagesStorage).length) return null;
-  return (
-    <ListMessagesWrapper onScroll={() => setShowPopup(false)}>    
-      {Object.values(messagesStorage).map(({profile,messages}) => (
-        <MessageItem key={profile._id} messenger={profile} latestTime={messages[0].createdAt}/>
-      ))}      
-    </ListMessagesWrapper>
-  )
-}
+  const {
+    data: { currentChat },
+  } = useQuery(GET_CURRENT_CHAT, { fetchPolicy: "cache-only" });
+  const {
+    data: { messagesStorage },
+  } = useQuery(GET_MESSAGES_STORAGE, { fetchPolicy: "cache-and-network" });
+  const { refetch: fetchFriends } = useQuery(FETCH_FRIENDS, { skip: true });
 
-export default ListMessages
+  const { setShowPopup } = usePopupMessagesActions();
+
+  if (!Object.keys(messagesStorage).length) return null;
+  return (
+    <ListMessagesWrapper onScroll={() => setShowPopup(false)}>
+      {Object.values(messagesStorage).map(({ profile, messages }) => (
+        <MessageItem
+          key={profile._id}
+          messenger={profile}
+          latestTime={messages[0].createdAt}
+          active={currentChat && currentChat._id === profile._id}
+        />
+      ))}
+    </ListMessagesWrapper>
+  );
+};
+
+export default React.memo(ListMessages);
